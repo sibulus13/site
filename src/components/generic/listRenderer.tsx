8fa1f66ef768dcf6f498fc8936bcdc61a4ea32b7
Si8n <chengjie.michael.huang@gmail.com>
2023-10-04 16:19:57 -0700

    animated skills icons

--- src/components/generic/listRenderer.tsx
+++ src/components/generic/listRenderer.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 export default function listRenderer(items: any[]) {
   return (
     <div className="flex pt-2 flex-wrap justify-center">
@@ -6,7 +8,21 @@ export default function listRenderer(items: any[]) {
           className="px-4 pb-2 flex flex-col items-center text-center"
           key={index}
         >
-          <div>{item.icon}</div>
+          <motion.div
+            animate={{
+              rotate: [0, -5, -10, -5, 0, 5, 10, 5, 0],
+              scale: [1, 1.2, 1.4, 1.2, 1, 0.8, 0.6, 0.8, 1],
+              transition: { duration: 0.7, delay: 0.3 * index },
+            }}
+            initial={{ rotate: 0, scale: 1 }}
+            whileHover={{
+              rotate: [0, -5, -10, -5, 0, 5, 10, 5, 0],
+              scale: [1, 1.2, 1.4, 1.2, 1, 0.8, 0.6, 0.8, 1],
+              transition: { duration: 0.3},
+            }}
+          >
+            {item.icon}
+          </motion.div>
           <p>{item.name}</p>
         </div>
       ))}
