52baf245b64633c5df7b866ea02188a8ea9b8c32
sib <chengjie.michael.huang@gmail.com>
2023-10-24 09:35:53 -0700

    testing new styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,23 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
+    // fontSize: {
+    //   sm: '0.8rem',
+    //   base: '1.5rem',
+    //   xl: '1.75rem',
+    //   '2xl': '1.563rem',
+    //   '3xl': '1.953rem',
+    //   '4xl': '2.441rem',
+    //   '5xl': '3.052rem',
+    // },
+      extend: {
+        colors: {
+          my_color: '#4dcb7a',
+      },
+      gradientColorStopPositions: {
+        33: '33%',
+        66: '66%',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
