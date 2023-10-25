d4be2a013656147abfa7a6519da3c92e1b4f5f4e
sib <chengjie.michael.huang@gmail.com>
2023-10-25 10:49:36 -0700

    ...

--- src/style/main.tsx
+++ src/style/main.tsx
@@ -6,7 +6,7 @@ export const accentColor = "text-slate-300 ";
 
 export const h1 = primaryColor + "text-4xl md:text-6xl ";
 
-export const h2 = "text-3xl md:text-4xl pt-6 pb-1";
+export const h2 = "text-3xl md:text-4xl pt-6 pb-1 ";
 
 export const date = secondaryColor + " ";
 
@@ -19,4 +19,4 @@ export const fullPage = "h-screen min-h-screen flex flex-col ";
 export const clickable = 'rounded-3xl border border-white p-1 px-3 bg-gray-950 ';
 
 export const mainGradient =
-  "bg-gradient-to-r from-gray-300 to-purple-400 inline-block text-transparent bg-clip-text";
+  "bg-gradient-to-r from-gray-300 to-purple-400 inline-block text-transparent bg-clip-text ";
