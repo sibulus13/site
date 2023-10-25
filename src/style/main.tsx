f44db3808fa7e3cf35dc5cf33008e9015cc9a3fa
sib <chengjie.michael.huang@gmail.com>
2023-10-25 10:28:25 -0700

    More padding

--- src/style/main.tsx
+++ src/style/main.tsx
@@ -10,7 +10,7 @@ export const h2 = "text-3xl md:text-4xl ";
 
 export const date = secondaryColor + " ";
 
-export const p = accentColor + "text-xl md:text-2xl ";
+export const p = accentColor + "text-xl md:text-2xl p-1";
 
 export const subp = "text-lg md:text-xl ";
 
