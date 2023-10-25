38cce004c85cabc5f1ce11c6a983ed90e8acff8f
sib <chengjie.michael.huang@gmail.com>
2023-10-25 10:24:56 -0700

    Padding latest

--- src/components/Home/Latest.tsx
+++ src/components/Home/Latest.tsx
@@ -21,44 +21,50 @@ export default async function Latest({ type, CTA, hrefMore, titleID }) {
       id={titleID}
       className="h-screen min-h-screen flex flex-col snap-start"
     >
-      <h1 className={Main.h1 + "pt-14 md:pl-20"}>
-        Latest <span className="capitalize ">{type}</span>
-      </h1>
-      <div className="flex flex-col h-full md:w-1/3 md:mx-auto">
-        <Link
-          className="grow m-2 p-2 flex flex-col"
-          href={{
-            pathname: `${hrefMore}/` + title,
-            query: {
-              id: id,
-            },
-          }}
-        >
-          <div className="relative h-1/2 grow">
-            <Image
-              src={"https:" + url}
-              alt={title}
-              fill
-              className="object-cover border rounded-xl"
-            ></Image>
+      <div className="h-5/6">
+        <h1 className={Main.h1 + "pt-14 md:pl-20"}>
+          Latest <span className="capitalize ">{type}</span>
+        </h1>
+        <div className="flex flex-col h-full md:w-1/3 md:mx-auto">
+          <Link
+            className="grow m-2 p-2 flex flex-col"
+            href={{
+              pathname: `${hrefMore}/` + title,
+              query: {
+                id: id,
+              },
+            }}
+          >
+            <div className="relative h-1/2 grow">
+              <Image
+                src={"https:" + url}
+                alt={title}
+                fill
+                className="object-cover border rounded-xl"
+              ></Image>
+            </div>
+            <div className="pt-4">
+              <h2 className={Main.h2}>{title}</h2>
+              <p className={Main.date + "text-right"}>
+                {new Date(date).toDateString()}
+              </p>
+              <p className={Main.p}>{description}</p>
+            </div>
+          </Link>
+          <div>
+            <MotionButton
+              clickable
+              className={"flex justify-end pb-8"}
+              href={hrefMore}
+            >
+              <span>
+                More {type}
+                {type[type.length - 1] === "s" ? "" : "s"}
+              </span>
+            </MotionButton>
           </div>
-          <div className="pt-4">
-            <h2 className={Main.h2}>{title}</h2>
-            <p className={Main.date + "text-right"}>
-              {new Date(date).toDateString()}
-            </p>
-            <p className={Main.p}>{description}</p>
-          </div>
-        </Link>
-        <div>
-          <MotionButton clickable className={"flex justify-end pb-8"} href={hrefMore}>
-            <span>
-              More {type}
-              {type[type.length - 1] === "s" ? "" : "s"}
-            </span>
-          </MotionButton>
+          {CTA}
         </div>
-        {CTA}
       </div>
     </div>
   );
