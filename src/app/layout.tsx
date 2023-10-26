ec3c91941c2a3d66901d22b87207d658876e322e
sib <chengjie.michael.huang@gmail.com>
2023-10-26 08:42:38 -0700

    Bg rad grad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body>
+      <body className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] to-black from-slate-950">
         <NavBar></NavBar>
         <div className="px-4 pt-14">{children}</div>
         <Analytics />
