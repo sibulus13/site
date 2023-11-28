8cafe9356209dd8f6e05c1770032365d6f530550
Si8n <chengjie.michael.huang@gmail.com>
2023-11-28 11:21:36 -0800

    Refactor contactCTA and adventureCTA components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,8 +35,18 @@ export const metadata: Metadata = {
   title: "Home",
 };
 
+const adventureCTA = (
+  <MotionButton
+    clickable
+    className="flex justify-center"
+    href="#latestAdventure"
+  >
+    See what I have been up to
+  </MotionButton>
+);
+
 const contactCTA = (
-  <div className={Main.p + "mx-auto align-text-bottom pb-2 pt-8"}>
+  <div className={Main.p + "mx-auto align-text-bottom"}>
     Lets develop the future together
     <div className="flex justify-center gap-4 text-3xl pt-2">
       <MotionButton clickable href="https://www.linkedin.com/in/sibulus0/">
@@ -50,14 +60,4 @@ const contactCTA = (
       </MotionButton>
     </div>
   </div>
-);
-
-const adventureCTA = (
-  <MotionButton
-    clickable
-    className={"flex justify-center pb-2"}
-    href="#latestAdventure"
-  >
-    See what I have been up to
-  </MotionButton>
 );
