5b7d7053985e4c3e23d231a753e5a7b7c761d973
Si8n <chengjie.michael.huang@gmail.com>
2023-11-28 11:05:48 -0800

    Refactor Home Hero component

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -15,104 +15,97 @@ export default function Hero() {
 
   return (
     <div className={Main.fullPage}>
-      <div className="h-5/6 md:h-5/6">
-        <div className="h-5/6 flex flex-col justify-center md:flex-row">
-          {/* Text container */}
-          <div className="flex flex-col justify-center md:pr-4 md:w-1/3">
-            <h1 className={Main.h1}>
-              Hi, I am{" "}
-              <span className="bg-gradient-to-r from-gray-300 to-purple-400 inline-block text-transparent bg-clip-text">
-                Michael
-              </span>
-              ,
-            </h1>
-            <div className="flex text-xs gap-2 pt-1 md:pt-2">
-              {titles.map((title, index) => (
-                <p
-                  key={index}
-                  className="border rounded px-1 whitespace-nowrap"
-                >
-                  {title}
-                </p>
-              ))}
-            </div>
-            <div>
-              <p className={Main.p + "pt-2"}>
-                currently tinkering towards a better tomorrow.
+      {/* Intro */}
+      <div className="h-5/6 flex flex-col justify-center md:flex-row">
+        {/* Text container */}
+        <div className="flex flex-col justify-center md:pr-4 md:w-1/3">
+          <h1 className={Main.h1}>
+            Hi, I am{" "}
+            <span className="bg-gradient-to-r from-gray-300 to-purple-400 inline-block text-transparent bg-clip-text">
+              Michael
+            </span>
+            ,
+          </h1>
+          <div className="flex text-xs gap-2 pt-1 md:pt-2">
+            {titles.map((title, index) => (
+              <p key={index} className="border rounded px-1 whitespace-nowrap">
+                {title}
               </p>
-              <p className={Main.subp}>
-                {" "}
-                {">"} 3+ years of Software Engineering Experience.
-              </p>
-              <p className={Main.subp}>
-                {" "}
-                {">"} BAsc Mechatronic Systems Engineering / Computer Science
-                (AI) Minor.
-              </p>
-              <p className={Main.subp + "py-4"}>
-                Armed with the power of technology to fight for a better world,
-                I am passionate about Full Stack development, automation and
-                data driven optimization.
-              </p>
-            </div>
+            ))}
           </div>
-          {/* Image Container */}
-          <div className="flex flex-col justify-center h-1/2 md:h-full md:w-1/3">
-            <div className="relative h-full md:h-2/3">
-              <Image
-                src={cat_typing_img}
-                alt="cat typing"
-                fill
-                className="object-contain rounded-b-full"
-              ></Image>
-            </div>
-            <br></br>
-            <MotionButton
-              href="/AboutMe"
-              clickable
-              className={"flex justify-end py-1 md:justify-center"}
-            >
-              More about me
-            </MotionButton>
+          <div>
+            <p className={Main.p + "pt-2"}>
+              currently tinkering towards a better tomorrow.
+            </p>
+            <p className={Main.subp}>
+              {" "}
+              {">"} 3+ years of Software Engineering Experience.
+            </p>
+            <p className={Main.subp}>
+              {" "}
+              {">"} BAsc Mechatronic Systems Engineering / Computer Science (AI)
+              Minor.
+            </p>
+            <p className={Main.subp + "py-2"}>
+              Armed with the power of technology to fight for a better world, I
+              am passionate about Full Stack development, automation and data
+              driven optimization.
+            </p>
           </div>
         </div>
-        {/* Link Buttons */}
-        <div className={Main.p + "mx-auto align-text-bottom py-1"}>
-          <div className="flex justify-center gap-2 p-4">
-            <MotionButton clickable onClick={saveResume}>
-              <div className="flex gap-1 items-center">
-                <GrDocumentPdf></GrDocumentPdf> Resume
-              </div>
-            </MotionButton>
-            <MotionButton
-              clickable
-              href="https://www.linkedin.com/in/sibulus0/"
-            >
-              <div className="flex gap-1 items-center">
-                <BiLogoLinkedin></BiLogoLinkedin>
-                LinkedIn
-              </div>
-            </MotionButton>
-            <MotionButton
-              clickable
-              href="https://github.com/sibulus13"
-              className="text-3xl"
-            >
-              <div className="flex gap-1 items-center">
-                <BiLogoGithub></BiLogoGithub>
-                GitHub
-              </div>
-            </MotionButton>
+        {/* Image Container */}
+        <div className="flex flex-col justify-center h-1/2 md:h-full md:w-1/3">
+          <div className="relative h-full md:h-2/3">
+            <Image
+              src={cat_typing_img}
+              alt="cat typing"
+              fill
+              className="object-contain rounded-b-full"
+            ></Image>
           </div>
+          <br></br>
+          <MotionButton
+            href="/AboutMe"
+            clickable
+            className={"flex justify-end py-1 md:justify-center"}
+          >
+            More about me
+          </MotionButton>
         </div>
-        <MotionButton
-          clickable
-          className={"flex justify-center pb-20"}
-          href="#latestProject"
-        >
-          Check out my latest work
-        </MotionButton>
       </div>
+      {/* Link Buttons */}
+      <div className={Main.p + "mx-auto align-text-bottom py-1"}>
+        <div className="flex justify-center gap-2 p-4">
+          <MotionButton clickable onClick={saveResume}>
+            <div className="flex gap-1 items-center">
+              <GrDocumentPdf></GrDocumentPdf> Resume
+            </div>
+          </MotionButton>
+          <MotionButton clickable href="https://www.linkedin.com/in/sibulus0/">
+            <div className="flex gap-1 items-center">
+              <BiLogoLinkedin></BiLogoLinkedin>
+              LinkedIn
+            </div>
+          </MotionButton>
+          <MotionButton
+            clickable
+            href="https://github.com/sibulus13"
+            className="text-3xl"
+          >
+            <div className="flex gap-1 items-center">
+              <BiLogoGithub></BiLogoGithub>
+              GitHub
+            </div>
+          </MotionButton>
+        </div>
+      </div>
+      <MotionButton
+        clickable
+        className={"flex justify-center pb-20"}
+        href="#latestProject"
+      >
+        Check out my latest work
+      </MotionButton>
     </div>
   );
 }
