850b9eb3431ee3a0c51e317692277c63ef8235a2
Sib <chengjie.michael.huang@gmail.com>
2023-12-10 12:46:16 -0800

    Removed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import NavBar from "../components/generic/NavBar";
-import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Metadata } from "next";
 
@@ -18,7 +17,6 @@ export default function RootLayout({
       <body className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] to-black from-slate-950">
         <NavBar></NavBar>
         <div className="px-4 pt-14">{children}</div>
-        <Analytics />
         <SpeedInsights />
       </body>
     </html>
